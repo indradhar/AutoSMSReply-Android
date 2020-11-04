ea1d77e59c84a9eddacbb2ab9d62bfec5857fc38
indradhar <indradhar.paka.18mec@bmu.edu.in>
2020-11-05 02:17:31 +0530

    Any User Any Message commit

---
+++ Project_v1/app/src/main/java/com/example/project_v1/DialogueAllUser.java
@@ -0,0 +1,74 @@
+package com.example.project_v1;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class DialogueAllUser extends DialogFragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View view=inflater.inflate(R.layout.dialogue_layout, container, false);
+        EditText editText=(EditText)view.findViewById(R.id.edit1);
+        TextView textView=(TextView)view.findViewById(R.id.view2);
+        TextView textView1=(TextView)view.findViewById(R.id.view3);
+
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message=editText.getText().toString();
+                if(message.length()!=0)
+                {
+                    saveDatabase(message);
+                }
+                else if(message.length()==0)
+                {
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run()
+                        {
+                            Toast.makeText(getActivity(), "Enter Any Message", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+                getDialog().dismiss();
+            }
+        });
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getDialog().dismiss();
+            }
+        });
+        return view;
+    }
+    void saveDatabase(String message)
+    {
+        SQLiteDatabase db;
+        db = getActivity().openOrCreateDatabase("DATABASE_MESSAGES",MODE_PRIVATE,null);
+        db.execSQL("Create table if not exists messages (outgoingmessage varchar(100))");
+
+        try
+        {
+            db.execSQL("insert into messages(outgoingmessage) values('"+message+"')");
+            Toast.makeText(getActivity(), "Message inserted", Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(getActivity(), "Error : "+e, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
--- Project_v1/app/src/main/java/com/example/project_v1/MainActivity.java
+++ Project_v1/app/src/main/java/com/example/project_v1/MainActivity.java
@@ -1,8 +1,20 @@
 package com.example.project_v1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +22,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= 26)//android 8.0
+        {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, "android.permission.RECEIVE_SMS") != PackageManager.PERMISSION_GRANTED)
+            {
+                ActivityCompat.requestPermissions(MainActivity.this,new String[]{"android.permission.RECEIVE_SMS","android.permission.SEND_SMS"},1);
+            }
+        }
+        Button button = (Button)findViewById(R.id.btn1);
+        Button button1 = findViewById(R.id.btn2);
+        Button button2 = findViewById(R.id.btn3);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startCustomUsers();
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startAllUsers();
+
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                viewDatabase();
+            }
+        });
+
+    }
+    void startCustomUsers()
+    {
+
+    }
+    void startAllUsers()
+    {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        AllUser frame1 = new AllUser();
+        fragmentTransaction.replace(R.id.frame1,frame1);
+        fragmentTransaction.commit();
+    }
+    void viewDatabase()
+    {
+
 
     }
 }
